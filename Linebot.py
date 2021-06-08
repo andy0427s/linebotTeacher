39cdf60a7f857c8195660452ca3537988c4dc0d5
andy0427s <andy0427s@gmail.com>
2021-06-08 14:28:44 +0800

    Update Linebot.py

--- Linebot.py
+++ Linebot.py
@@ -1,74 +1,5 @@
-# # import flask related
-# from flask import Flask, request, abort
-# # import linebot related
-# from linebot import (
-#     LineBotApi, WebhookHandler
-# )
-# from linebot.exceptions import (
-#     InvalidSignatureError
-# )
-# from linebot.models import (
-#     MessageEvent, TextMessage, TextSendMessage
-# )
+# 匯入所需模組
 
-# # import for database (SQLalchemy)
-
-
-
-
-
-
-# # create flask server
-# app = Flask(__name__)
-
-# # LINE 聊天機器人的基本資料
-
-# # Johnson linebot message API - Channel access token (from LINE Developer)
-# line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
-# # Johnson linebot message API - Channel secret
-# handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
-
-
-# # connect to database(SQLalchemy)
-
-
-
-# # 接收 LINE 的資訊
-
-# @app.route("/callback", methods=['POST'])
-# def callback():
-#     # get X-Line-Signature header value
-#     signature = request.headers['X-Line-Signature']
-
-#     # get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
-
-#     # handle webhook body
-#     try:
-#         # print('receive msg') - Testing
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         # print("Invalid signature. Please check your channel access token/channel secret.") -Testing
-#         abort(400)
-#     return 'OK'
-
-
-# # Our main code (handle msg) 
-
-
-
-
-
-
-
-
-
-# # run app
-# if __name__ == "__main__":
-#     app.run(host='', port=) # change to Heroku port/host
-
-##############################################################################################################################
 import os
 from datetime import datetime
 
@@ -79,23 +10,44 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
+
+# import for database (SQLalchemy)
+
+
+
+# LINE 聊天機器人的基本資料
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
 
+# 瀏覽器介面for Teacher to review (if needed)
+
+@app.route('/rec', methods=['GET', 'POST'])
+def get_file():
+    if request.method == "GET":
+        return render_template('file.html', page_header="upload hand write picture")
+    elif request.method == "POST":
+        file = request.files['file']
+        if file:
+            filename = str(uuid.uuid4())+"_"+file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            predict = model.recog_digit(filename)
+        return render_template('recog_result.html', page_header="hand writing digit recognition", predict = predict, src = url_for('static', filename=f'uploaded/{filename}'))
+
+
+# Linebot part 
+
 @app.route("/", methods=["GET", "POST"])
 def callback():
 
     if request.method == "GET":
-        # return "Hello Heroku"
-        return render_template(('index.html',
-                           page_header="page_header",
-                           current_time=datetime.utcnow()))
+        return "Hello Heroku"
 
 
-    elif request.method == "POST":
+    if request.method == "POST":
         signature = request.headers["X-Line-Signature"]
         body = request.get_data(as_text=True)
 
@@ -107,6 +59,8 @@ def callback():
         return "OK"
 
 
+# 接收 LINE 的資訊 / 回傳相同資訊
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
@@ -114,3 +68,9 @@ def handle_message(event):
     # Send To Line
     reply = TextSendMessage(text=f"{get_message}")
     line_bot_api.reply_message(event.reply_token, reply)
+
+
+
+# Run app on Heroku
+if __name__ == "__main__":
+    app.run(debug=True)
