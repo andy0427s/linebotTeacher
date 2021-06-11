b15d6cb8142c1519d41a99ff6005181a156311d2
Johnson Chou <johnsonchou3@gmail.com>
2021-06-11 19:50:06 +0800

    1

--- app.py
+++ app.py
@@ -181,15 +181,15 @@ def addData():
 def reset():
     db.drop_all()
     db.create_all()
-    s1 = Student(sId=1, sName="Bob", lineId="e109bs")
-    s2 = Student(sId=2, sName="Alice", lineId="a983g")
-    s3 = Student(sId=3, sName="Charlie", lineId="f027k")
-    s4 = Student(sId=4, sName="Dylan", lineId="m410p")
-    a1 = Assignment(prompt="You should go to the store")
-    a2 = Assignment(prompt="He finished his breakfast early")
-    a3 = Assignment(prompt="The flowers bloomed early this year")
-    h1 = Homework(aId=2, lineId='f027k', file="/uploaded/zzz.wav")
-    entries = [s1]
+    s1 = [Student(sId=1, sName="Bob", lineId="e109bs")]
+    s2 = [Student(sId=2, sName="Alice", lineId="a983g")]
+    s3 = [Student(sId=3, sName="Charlie", lineId="f027k")]
+    s4 = [Student(sId=4, sName="Dylan", lineId="m410p")]
+    a1 = [Assignment(prompt="You should go to the store")]
+    a2 = [Assignment(prompt="He finished his breakfast early")]
+    a3 = [Assignment(prompt="The flowers bloomed early this year")]
+    h1 = [Homework(aId=2, lineId='f027k', file="/uploaded/zzz.wav")]
+    entries = [s1, s2, s3, s4, a1, a2, a3, h1]
     db.session.add_all(entries)
     db.session.commit()
     return redirect('/showtables')
